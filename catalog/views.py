a32c9adc3dbe7b327101e425faa8bd21c071c532
Roman Feoktistov <romafeoktistov942@gmail.com>
2024-09-12 20:47:28 +0300

    Refactor home.html, contacts.html, and views.py; update URLs and settings.

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-def home(request):
-    return render(request, 'home.html')
+from .models import Product
 
-def contacts(request):
-    return render(request, 'contacts.html')
+def products_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'products_list.html', context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'product_detail.html', context)
