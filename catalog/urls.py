9c0a6cde4d2a9b36db98522049d7465e01789147
Roman Feoktistov <romafeoktistov942@gmail.com>
2024-09-12 20:47:16 +0300

    Refactor home.html and contacts.html, update views, and adjust URLs for product views.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import products_list, product_detail
 
-app_name = CatalogConfig
+app_name = 'catalog'
 
 urlpatterns = [
-    path("", home, name="home"),
-    path("", contacts, name="contacts"),
+    path("", products_list, name="products_list"),
+    path("products/<int:pk>/", product_detail, name="product_detail"),
 ]
