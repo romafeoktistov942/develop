9a30cac07a0ff8a96086ad2628735ef779f0d0df
Roman Feoktistov <romafeoktistov942@gmail.com>
2024-10-06 17:04:16 +0300

    added save and create methods to Version model and set_default_version signal handler

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 class Category(models.Model):
@@ -58,6 +60,9 @@ class Product(models.Model):
     views_counter = models.PositiveBigIntegerField(
         verbose_name="Количество просмотров", default=0
     )
+    @property
+    def active_versions(self):
+        return self.versions.filter(is_active=True)
 
     class Meta:
         verbose_name = "Товар"
@@ -70,22 +75,53 @@ class Product(models.Model):
 class Version(models.Model):
     product = models.ForeignKey(
         Product,
-        related_name="product",
-        on_delete=models.SET_NULL,
+        related_name="versions",
+        on_delete=models.CASCADE,
         null=True,
         blank=True,
         verbose_name="Продукт",
     )
 
-    number = models.PositiveIntegerField(verbose_name="Номер версии")
+    version_number = models.PositiveIntegerField(verbose_name="Номер версии")
 
-    name = models.CharField(max_length=150, verbose_name="Название версии")
+    version_name = models.CharField(max_length=150, verbose_name="Название версии")
 
     is_active = models.BooleanField(verbose_name="Признак текущей версии")
+   
+    def save(self, *args, **kwargs):
+        print("Сохраняем версию...")
+        if not self.product.versions.filter(is_active=True).exists():
+            print("Нет активных версий, устанавливаем в True...")
+            self.is_active = True
+        super().save(*args, **kwargs)
+        print("Версия сохранена...")
+
+    @classmethod
+    def create(cls, **kwargs):
+        print("Создаем новую версию...")
+        instance = cls(**kwargs)
+        instance.save()
+        print("Версия создана...")
+        return instance
 
     class Meta:
         verbose_name = "Версия"
         verbose_name_plural = "Версии"
 
     def __str__(self):
-        return f"{self.number} - {self.name}"
+        return f"{self.product} {self.version_number} {self.version_name} {self.is_active}"
+    
+    @receiver(post_save, sender=Product)
+    def set_default_version(sender, instance, created, **kwargs):
+        if created and not instance.versions.exists():
+            # Создаем новую версию по умолчанию для продукта
+            Version.objects.create(
+                product=instance,
+                version_number=1,
+                version_name='Версия по умолчанию',
+                is_active=True
+            )
+        elif instance.versions.exists() and not instance.versions.filter(is_active=True).exists():
+            # Если нет активных версий, устанавливаем последнюю версию как активную
+            instance.versions.last().is_active = True
+            instance.versions.last().save()
