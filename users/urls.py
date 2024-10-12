387f6401ea47d8758213edf175af376a6f50b1af
Roman Feoktistov <romafeoktistov942@gmail.com>
2024-10-13 00:51:34 +0300

    Add user authentication views and email verification functionality.

---
+++ users/urls.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+from users.apps import UsersConfig
+from users.views import PasswordResetView, UserCreateView, email_verification
+
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("register/", UserCreateView.as_view(), name="register"),
+    path("verification/<str:token>/", email_verification, name="verification"),
+    path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
+]
