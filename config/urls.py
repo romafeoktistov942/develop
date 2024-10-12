99f8b7ce6d3663dfd9a2bd6ef9eecce0135a0757
Roman Feoktistov <romafeoktistov942@gmail.com>
2024-10-13 00:50:31 +0300

    Add user URLs and login view to urlpatterns in main urls.py

--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("catalog.urls", namespace='catalog')),
+    path("users/", include("users.urls", namespace='users')),
     path('blog/', include('blog.urls', namespace='blog')),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
