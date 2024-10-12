f35ae48ff3146fd72d6616080f7d11352fbadaa8
Roman Feoktistov <romafeoktistov942@gmail.com>
2024-10-13 00:50:54 +0300

    Add user authentication, registration, and password reset functionality.

---
+++ users/forms.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.forms import UserCreationForm
+
+from django import forms
+from catalog.forms import StyleFormMixin
+from users.models import User
+
+
+class UserRegisterForm(StyleFormMixin, UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("email", "password1", "password2")
+
+
+class PasswordResetForm(forms.Form):
+    email = forms.EmailField(label='Email')
